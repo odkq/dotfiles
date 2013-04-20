656770427f597ccbd92df4f626899df96448fe53
odkq <pablo@odkq.com>
2013-04-20 13:44:45 +0200

    Disable touchpad

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -2,10 +2,15 @@
 xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1 &
 xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 8 2 &
 xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 8 6 7 4 5 &
+# Disable touchpad on the Thinpad T400 (probably in others as well)
+# I disable this because i always use the trackpoint
+# but sometimes i touch it by mistake
+synclient TouchpadOff=1
 # For volume keys and such
 xbindkeys &
 # Boot Jamie Zawinsky's legacy
 xscreensaver &
 # A panel for good times (see .config/fbpanel/default for configuration)
 TZ='Europe/Madrid' fbpanel &
-
+# What's XFree86 worth without an xterm?
+xterm &
