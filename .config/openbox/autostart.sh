2a7959ef3defebe9bf07ae725ed9069c0f464032
odkq <pablo@odkq.com>
2013-06-10 00:52:04 +0200

    usb thinkpad trackpoint middle button scroll

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -2,6 +2,13 @@
 xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1 &
 xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 8 2 &
 xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 8 6 7 4 5 &
+# If you ever buy a thinkpad keyboard with trackpoint over usb (actually the
+# only way to get a trackpoint in a desktop computer) you will notice that the name
+# listed by xin
+# Lite-On Technology Corp. ThinkPad USB Keyboard with TrackPoint
+xinput set-int-prop "pointer:Lite-On Technology Corp. ThinkPad USB Keyboard with TrackPoint" "Evdev Wheel Emulation" 8 1 &
+xinput set-int-prop "pointer:Lite-On Technology Corp. ThinkPad USB Keyboard with TrackPoint" "Evdev Wheel Emulation Button" 8 2 &
+xinput set-int-prop "pointer:Lite-On Technology Corp. ThinkPad USB Keyboard with TrackPoint" "Evdev Wheel Emulation Axes" 8 6 7 4 5 &
 # Disable touchpad on the Thinpad T400 (probably in others as well)
 # I disable this because i always use the trackpoint
 # but sometimes i touch it by mistake
